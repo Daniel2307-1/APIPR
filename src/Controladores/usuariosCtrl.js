71fd10d64464580e29ffcfd3039d6ded1eaead0f
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-06-03 21:36:16 -0500

    Update usuariosCtrl.js

--- src/Controladores/usuariosCtrl.js
+++ src/Controladores/usuariosCtrl.js
@@ -20,3 +20,37 @@ export const obetenerdatos = async (req, res) => {
         });
     }
 };
+export const obetenerdatosA = async (req, res) => {
+  const { usuario, clave } = req.params;
+  try {
+    let [result] = await sql.query(
+      'SELECT * FROM usuarios WHERE alias = ? AND contrasena = ? LIMIT 1',
+      [usuario, clave]
+    );
+    if (result.length === 0) {
+      [result] = await sql.query(
+        'SELECT * FROM usuarios WHERE correo = ? AND contrasena = ? LIMIT 1',
+        [usuario, clave]
+      );
+    }
+    if (result.length > 0) {
+      const usuarioData = result[0];
+
+      res.json({
+        success: true,
+        usuario: usuarioData
+      });
+    } else {
+      res.json({
+        success: false,
+        message: 'Usuario, correo o clave incorrectos'
+      });
+    }
+  } catch (error) {
+    console.error('Error en login:', error);
+    return res.status(500).json({
+      message: "Error en el servidor",
+      error: error.message
+    });
+  }
+};
