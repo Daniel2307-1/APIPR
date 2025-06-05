a357b751fc08d9de1dd5c127d41fd0c39b1955ca
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-06-05 00:18:40 -0500

    Create LikeCtrl.js

---
+++ src/Controladores/LikeCtrl.js
@@ -0,0 +1,83 @@
+import { sql } from '../bd.js';
+
+export const enviarmensajedebasededatos = (req, res) => {
+    res.send('Lista de like');
+};
+export const obetenerdatosporusuario = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const [result] = await sql.query('SELECT *FROM LikeT WHERE id_usuario = ?', [id]);
+
+    if (result.length <= 0) {
+      return res.status(404).json({
+        id: 0,
+        message: "Favorito no encontrado"
+      });
+    }
+
+ res.json({ cant: result.length, data: result });
+  } catch (error) {
+    console.error("Error al obtener el favorito:", error);
+    return res.status(500).json({ message: "Error en el servidor" });
+  }
+};
+export const postLike = async (req, res) => {
+  try {
+    const { id_usuario, id_lenguaje } = req.body;
+
+    if (!id_usuario || !id_lenguaje) {
+      return res.status(400).json({ message: "Faltan campos obligatorios" });
+    }
+
+    const [result] = await sql.query(
+      `INSERT INTO LikeT (id_usuario, id_lenguaje) VALUES (?, ?)`,
+      [id_usuario, id_lenguaje]
+    );
+
+    res.status(201).json({
+      id_favorito: result.insertId,
+      message: "Me gusta añadido a Like"
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Error en el servidor",
+      error: {
+        message: error.message,
+        code: error.code,
+        stack: error.stack
+      }
+    });
+  }
+};
+export const deleteLike = async (req, res) => {
+  try {
+    const { id_usuario, id_lenguaje } = req.params;
+
+    const [result] = await sql.query(
+      'DELETE FROM LikeT WHERE id_usuario = ? AND id_lenguaje = ?',
+      [id_usuario, id_lenguaje]
+    );
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({
+        message: "Me gusta no encontrado"
+      });
+    }
+
+    res.json({
+      message: "Me gusta eliminado correctamente"
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Error en el servidor",
+      error: {
+        message: error.message,
+        code: error.code,
+        stack: error.stack
+      }
+    });
+  }
+};
