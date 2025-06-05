d6d779ca3ff15a0bfc08993b01985a9381ae25cb
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-06-04 22:56:55 -0500

    Update Favoritos.routes.js

--- src/Routes/Favoritos.routes.js
+++ src/Routes/Favoritos.routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import {obetenerdatosporusuario,postFavoritos } from '../Controladores/FavoritosCtrl.js'
+import {obetenerdatosporusuario,postFavoritos,deleteFavorito} from '../Controladores/FavoritosCtrl.js'
 const router = express.Router();
 router.get('/favoritos/:id',obetenerdatosporusuario)
 router.post('/favoritosA/',postFavoritos)
-
+router.delete('/favoritosE/:id_usuario/:id_lenguaje', deleteFavorito);
 export default router
