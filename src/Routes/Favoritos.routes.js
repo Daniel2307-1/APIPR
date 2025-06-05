7fc54d1dbfa25e30f78190c169e87c2ae3768e02
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-06-04 22:52:01 -0500

    Update Favoritos.routes.js

--- src/Routes/Favoritos.routes.js
+++ src/Routes/Favoritos.routes.js
@@ -1,5 +1,7 @@
 import express from 'express';
-import {obetenerdatosporusuario } from '../Controladores/FavoritosCtrl.js'
+import {obetenerdatosporusuario,postFavoritos } from '../Controladores/FavoritosCtrl.js'
 const router = express.Router();
 router.get('/favoritos/:id',obetenerdatosporusuario)
+router.post('/favoritosA/',postFavoritos)
+
 export default router
