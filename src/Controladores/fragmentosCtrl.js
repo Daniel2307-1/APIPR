74ff5c498d9709cdad8733b60f5089d8debe8196
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-06-06 01:28:29 -0500

    Update fragmentosCtrl.js

--- src/Controladores/fragmentosCtrl.js
+++ src/Controladores/fragmentosCtrl.js
@@ -6,10 +6,10 @@ export const enviarmensajedebasededatos = (req, res) => {
 export const obtenerfragmentosxid = async (req, res) => {
     try {
         const ID = req.params.id;
-        const [result] = await sql.query('select * from productos where prod_id=?', [ID]);
+        const [result] = await sql.query('select * from fragmentos where id_lenguaje=?', [ID]);
         if (result.length <= 0) return res.status(400).json({
             cli_id: 0,
-            message: "producto no encontrado"
+            message: "fragmento no encontrado"
         });
         res.json(result[0]);
     } catch {
