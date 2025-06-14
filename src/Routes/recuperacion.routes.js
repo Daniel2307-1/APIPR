72d2a70f5e53c326b51cc4eb4108d4a1701b65fb
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-06-14 00:45:21 -0500

    Update recuperacion.routes.js

--- src/Routes/recuperacion.routes.js
+++ src/Routes/recuperacion.routes.js
@@ -1,12 +1,10 @@
 import express from 'express';
-import { crearRecuperacion, verificarCodigo } from '../Controladores/recuperacionCtrl.js';
+import { crearRecuperacion, verificarCodigo,almacenarcodigoRecuperacion} from '../Controladores/recuperacionCtrl.js';
 
 const router = express.Router();
 
-// Ruta para crear un código de recuperación
 router.post('/recuperacion/:id_usuario', crearRecuperacion);
-
-// Ruta para verificar el código de recuperación
 router.post('/recuperacion/verificar', verificarCodigo);
+router.get('/recuperacion/:id_usuario/:codigo', almacenarcodigoRecuperacion);
 
 export default router;
