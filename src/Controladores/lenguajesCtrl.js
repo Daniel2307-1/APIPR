1b536bcee5aed0e1abd3905336186553b05266bd
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-06-14 20:27:44 -0500

    Update lenguajesCtrl.js

--- src/Controladores/lenguajesCtrl.js
+++ src/Controladores/lenguajesCtrl.js
@@ -20,3 +20,19 @@ export const obetenerdatos = async (req, res) => {
         });
     }
 };
+export const obtenerlenguajeid = async (req, res) => {
+  try {
+    const ID = req.params.id;
+    const [result] = await sql.query('SELECT * FROM lenguajesP WHERE id_lenguaje = ?', [ID]);
+
+    if (result.length <= 0) {
+      return res.status(400).json({
+        cli_id: 0,
+        message: "lenguaje no encontrado"
+      });
+    }
+    res.json({ cant: result.length, data: result });
+  } catch {
+    return res.status(500).json({ message: "Error en el servidor" });
+  }
+};
