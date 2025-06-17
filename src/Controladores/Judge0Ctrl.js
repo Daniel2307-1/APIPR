7522c87735583040db2107f35c23a79ee39cfb72
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-06-17 12:24:20 -0500

    Update Judge0Ctrl.js

--- src/Controladores/Judge0Ctrl.js
+++ src/Controladores/Judge0Ctrl.js
@@ -4,7 +4,7 @@ import { validarCodigoConJudge0 } from '../Controladores/validateJudge0.js';
 // Asegúrate de que el nombre del fichero que exporta `validarCodigoConJudge0` esté bien
 
 export async function validarCodigo(req, res) {
-    console.log('Validar código - req.body:', req.body);
+  console.log('Validar código - req.body:', req.body);
 
   const { codigo, lenguaje, id_reto } = req.body;
 
@@ -14,12 +14,13 @@ export async function validarCodigo(req, res) {
 
   try {
     const resultados = await validarCodigoConJudge0(codigo, lenguaje, id_reto, sql);
+    console.log('Resultados:', resultados);
     const exito = resultados.every(r => r.correcto);
-
     res.json({ exito, resultados });
   } catch (error) {
-      console.error(error);
+    console.error('Error en validarCodigo:', error);
     res.status(500).json({ error: error.message });
   }
 }
+
 
