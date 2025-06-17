0e3c0b991ccd9962b16d5733a20e93919fcff5af
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-06-17 03:58:10 -0500

    Update Judge0.routes.ts

--- src/Routes/Judge0.routes.ts
+++ src/Routes/Judge0.routes.ts
@@ -1,23 +1,8 @@
 import express from 'express';
 const router = express.Router();
 
-router.post('/validar-codigo', async (req, res) => {
-  const { codigo, lenguaje, id_reto } = req.body;
+import { validarCodigo } from '../controllers/Judge0Ctrl.js';
 
-  if (!codigo || !lenguaje || !id_reto) {
-    return res.status(400).json({ error: 'Faltan datos' });
-  }
-
-  try {
-    const resultados = await validarCodigoConJudge0(codigo, lenguaje, id_reto, sql); // sql: tu conexión MySQL
-
-    const exito = resultados.every(r => r.correcto);
-
-    res.json({ exito, resultados });
-
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+router.post('/validar-codigo', validarCodigo);
 
 export default router;
