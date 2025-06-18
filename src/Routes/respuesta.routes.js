3437de85fefeaf3e35d5528d174f2ec83eefb824
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-06-18 00:21:24 -0500

    Update respuesta.routes.js

--- src/Routes/respuesta.routes.js
+++ src/Routes/respuesta.routes.js
@@ -2,6 +2,6 @@ import express from 'express';
 import {obetenerrespuestaporusuario,crearRespuesta} from '../Controladores/respuestaCtrl.js'
 const router = express.Router();
 router.get('/respuestaRp/:id',obetenerrespuestaporusuario)
-router.post('/respuesta/:id_usuario/:id_reto/:codigo_subido/:tipo', crearRespuesta);
+router.post('/respuesta/:id_usuario/:id_reto', crearRespuesta);
 
 export default router
