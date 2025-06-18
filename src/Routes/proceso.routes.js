377f458f60fd59aa0338829b81d3d4f077e05047
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-06-17 21:25:17 -0500

    Update proceso.routes.js

--- src/Routes/proceso.routes.js
+++ src/Routes/proceso.routes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import {obtenerProcesoPorId,crearProceso } from '../Controladores/procesoCtrl.js'
+import {obtenerProcesoPorId,crearProceso, actualizarProceso } from '../Controladores/procesoCtrl.js'
 
 const router = express.Router();
 
 router.get('/proceso/:id',obtenerProcesoPorId)
 router.post('/procesoA/:id',crearProceso)
-   
+router.put('/procesoU/:id', actualizarProceso); 
 export default router
