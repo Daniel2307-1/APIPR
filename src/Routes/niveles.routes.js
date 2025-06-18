b47e929240553ff97a8b259027e0f12d3e04ea96
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-06-18 01:12:13 -0500

    Update niveles.routes.js

--- src/Routes/niveles.routes.js
+++ src/Routes/niveles.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import {obtenerNivelPorId} from '../Controladores/nivelesCtrl.js'
+import {obtenerNivelPorId,obtenerNiveles} from '../Controladores/nivelesCtrl.js'
 
 const router = express.Router();
 
 router.get('/niveles/:id',obtenerNivelPorId)
+router.get('/niveles/',obtenerNiveles)
 
 export default router
