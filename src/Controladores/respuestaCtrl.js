652ce59586e82f960b0f672cb38118a7b271537c
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-06-18 00:31:42 -0500

    Update respuestaCtrl.js

--- src/Controladores/respuestaCtrl.js
+++ src/Controladores/respuestaCtrl.js
@@ -7,7 +7,26 @@ export const obetenerrespuestaporusuario = async (req, res) => {
   try {
     const id = req.params.id;
 
-    const [result] = await sql.query('SELECT *FROM respuesta WHERE id_usuario = ?', [id]);
+    const [result] = await sql.query('SELECT *FROM respuesta WHERE id_usuario = ? AND tipo=oficial', [id]);
+
+    if (result.length <= 0) {
+      return res.status(404).json({
+        id: 0,
+        message: "respuesta no encontrado"
+      });
+    }
+
+ res.json({ cant: result.length, data: result });
+  } catch (error) {
+    console.error("Error al obtener el respuesta:", error);
+    return res.status(500).json({ message: "Error en el servidor" });
+  }
+};
+export const obetenerrespuestaporusuario2 = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const [result] = await sql.query('SELECT *FROM respuesta WHERE id_usuario = ? AND tipo=usuario', [id]);
 
     if (result.length <= 0) {
       return res.status(404).json({
